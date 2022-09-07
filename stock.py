ce7b84e0188987a5e9df16d396a09371756eb86d
Hiyo1256 <68120102+Hiyo1256@users.noreply.github.com>
2022-09-07 05:09:08 +0400

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,81 @@
+import streamlit as st
+from datetime import date
+ 
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+
+START= "2015-01-01" #To change starting day
+TODAY=date.today().strftime("%Y-%m-%d") #To change current day
+
+st.title("Stock Prediction App by Aditya Kamat 12A") #title of app
+
+stocks = ("INFY","ICICIBANK.NS")
+selected_stock = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:",1,4)
+period = n_years*365
+
+@st.cache #we dont have to download again and again
+def load_data(ticker):
+    data = yf.download(ticker,START,TODAY)
+    data.reset_index(inplace=True)#Puts date in first column
+    return data
+
+data_load_state= st.text("Load data....")
+data = load_data(selected_stock)
+data_load_state.text("Loading data..... done!")
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='stock_open')) #Used to change data displayed on the axis
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='stock_open'))
+    fig.layout.update(title_text="Time Series Data",xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+# Forecasting
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date":"ds","Close":"y"})
+
+m = Prophet()
+m.fit(df_train) #Will fit training data and start training
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write('forecast data')
+fig1 = plot_plotly(m,forecast)
+st.plotly_chart(fig1)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
